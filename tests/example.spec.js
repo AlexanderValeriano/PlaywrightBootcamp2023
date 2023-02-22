4fbe1ac409a5936f5f27f32bc0b8cec9c1d899c3
valeriano alexander <valerianoalexander@gmail.com>
2023-02-22 19:08:31 +0100

    3 important CLI commands playwright

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,5 +1,6 @@
 const { test, expect } = require("@playwright/test");
-//npx playwright test tests/example.spec.js --headed
+//npx playwright test tests/example.spec.js --headed --browser=firefox
+//npx playwright test tests/example.spec.js --headed --browser=all
 test("First Test", async ({ page }) => {
   await page.goto("https://playwright.dev");
   await page.pause();
