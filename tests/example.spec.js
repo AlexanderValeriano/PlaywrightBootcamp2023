150d2700d03af2aa2121245e26f394d7f4c46f81
valeriano alexander <valerianoalexander@gmail.com>
2023-02-23 10:02:00 +0100

    178 Focusing a test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -16,3 +16,12 @@ test.only("Simple click test", async ({ page }) => {
   await element.click();
   await addElement.click();
 });
+
+test("Duplicate test", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com/");
+  // await page.pause();
+  const element = page.locator("a[href='/add_remove_elements/']");
+  const addElement = page.locator("button[onclick='addElement()']");
+  await element.click();
+  await addElement.click();
+});
