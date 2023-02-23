cedfc5511e689f3307c544c46d37a22f5d4cec44
valeriano alexander <valerianoalexander@gmail.com>
2023-02-23 10:47:42 +0100

    181 Grouping tests

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,41 +2,43 @@ const { test, expect } = require("@playwright/test");
 //npx playwright test tests/example.spec.js --headed --browser=firefox
 //npx playwright test tests/example.spec.js --headed --browser=all
 
-// npx playwright test --grep @smoke
-// npx playwright test --grep-invert @smoke / pour executer tous les test sauf le @smoke test
-test("First Test @smoke", async ({ page }) => {
-  await page.goto("https://playwright.dev");
-  // await page.pause();
-  const title = page.locator(".navbar__title.text--truncate");
-  await expect(title).toHaveText("Playwright");
-});
+test.describe("Smoke Tests", () => {
+  // npx playwright test --grep @smoke
+  // npx playwright test --grep-invert @smoke / pour executer tous les test sauf le @smoke test
+  test("First Test @smoke", async ({ page }) => {
+    await page.goto("https://playwright.dev");
+    // await page.pause();
+    const title = page.locator(".navbar__title.text--truncate");
+    await expect(title).toHaveText("Playwright");
+  });
 
-test("Simple click test @test-regression", async ({ page, browserName }) => {
-  // npx playwright test tests/example.spec.js --headed --browser=firefox
-  test.skip(browserName === "firefox", "Working on the firefox fix");
-  await page.goto("https://the-internet.herokuapp.com/");
-  // await page.pause();
-  const element = page.locator("a[href='/add_remove_elements/']");
-  const addElement = page.locator("button[onclick='addElement()']");
-  await element.click();
-  await addElement.click();
-});
+  test("Simple click test @test-regression", async ({ page, browserName }) => {
+    // npx playwright test tests/example.spec.js --headed --browser=firefox
+    test.skip(browserName === "firefox", "Working on the firefox fix");
+    await page.goto("https://the-internet.herokuapp.com/");
+    // await page.pause();
+    const element = page.locator("a[href='/add_remove_elements/']");
+    const addElement = page.locator("button[onclick='addElement()']");
+    await element.click();
+    await addElement.click();
+  });
 
-test("Duplicate test @regression-smoke", async ({ page }) => {
-  await page.goto("https://the-internet.herokuapp.com/");
-  // await page.pause();
-  const element = page.locator("a[href='/add_remove_elements/']");
-  const addElement = page.locator("button[onclick='addElement()']");
-  await element.click();
-  await addElement.click();
-});
+  test("Duplicate test @regression-smoke", async ({ page }) => {
+    await page.goto("https://the-internet.herokuapp.com/");
+    // await page.pause();
+    const element = page.locator("a[href='/add_remove_elements/']");
+    const addElement = page.locator("button[onclick='addElement()']");
+    await element.click();
+    await addElement.click();
+  });
 
-// npx playwright test --grep @regression
-test("Duplicate test @regression", async ({ page }) => {
-  await page.goto("https://the-internet.herokuapp.com/");
-  // await page.pause();
-  const element = page.locator("a[href='/add_remove_elements/']");
-  const addElement = page.locator("button[onclick='addElement()']");
-  await element.click();
-  await addElement.click();
+  // npx playwright test --grep @regression
+  test("Duplicate test @regression", async ({ page }) => {
+    await page.goto("https://the-internet.herokuapp.com/");
+    // await page.pause();
+    const element = page.locator("a[href='/add_remove_elements/']");
+    const addElement = page.locator("button[onclick='addElement()']");
+    await element.click();
+    await addElement.click();
+  });
 });
