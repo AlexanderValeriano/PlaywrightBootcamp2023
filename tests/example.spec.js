8e52ea755a2d085333e5036f86ae85d052a44e3a
valeriano alexander <valerianoalexander@gmail.com>
2023-02-23 10:12:00 +0100

    179 skipping a test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,14 +1,16 @@
 const { test, expect } = require("@playwright/test");
 //npx playwright test tests/example.spec.js --headed --browser=firefox
 //npx playwright test tests/example.spec.js --headed --browser=all
-test.skip("First Test", async ({ page }) => {
+test("First Test", async ({ page }) => {
   await page.goto("https://playwright.dev");
   // await page.pause();
   const title = page.locator(".navbar__title.text--truncate");
   await expect(title).toHaveText("Playwright");
 });
 
-test.only("Simple click test", async ({ page }) => {
+test("Simple click test", async ({ page, browserName }) => {
+  // npx playwright test tests/example.spec.js --headed --browser=firefox
+  test.skip(browserName === "firefox", "Working on the firefox fix");
   await page.goto("https://the-internet.herokuapp.com/");
   // await page.pause();
   const element = page.locator("a[href='/add_remove_elements/']");
