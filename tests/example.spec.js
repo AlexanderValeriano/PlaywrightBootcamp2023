571357e6479c161c0465b19c6c52f36f09727d8d
valeriano alexander <valerianoalexander@gmail.com>
2023-02-23 16:27:28 +0100

    188 beforeEach and afterEach Hooks

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -4,6 +4,13 @@ const { test, expect } = require("@playwright/test");
 // npx playwright test --config=playwright.config.js --project=Chrome
 
 test.describe("Smoke Tests", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+  });
+  test.afterEach(async ({ page }) => {
+    console.log("This test is finished");
+  });
+
   // npx playwright test --grep @smoke
   // npx playwright test --grep-invert @smoke / pour executer tous les test sauf le @smoke test
   test("First Test", async ({ page }) => {
@@ -16,16 +23,16 @@ test.describe("Smoke Tests", () => {
   test("Simple click test", async ({ page, browserName }) => {
     // npx playwright test tests/example.spec.js --headed --browser=firefox
     // test.skip(browserName === "firefox", "Working on the firefox fix");
-    await page.goto("/checkboxes");
-    await page.pause();
-    // await page.click("a[href='/add_remove_elements/']");
+    // await page.goto("/checkboxes");
+
+    // await page.pause();
+    await page.click("a[href='/add_remove_elements/']");
     // const locator1 = page.locator("text = Add/Remove Elementss");
     // await locator1.screenshot({ path: "screenshot2.png" });
     // await page.click("button[onclick='addElement()']");
   });
 
   test("Duplicate test 1", async ({ page }) => {
-    await page.goto("/");
     // await page.pause();
     const element = page.locator("a[href='/add_remove_elements/']");
     await element.click();
@@ -35,7 +42,6 @@ test.describe("Smoke Tests", () => {
 
   // npx playwright test --grep @regression
   test("Duplicate test 2", async ({ page }) => {
-    await page.goto("/");
     // await page.pause();
     const element = page.locator("a[href='/add_remove_elements/']");
     const addElement = page.locator("button[onclick='addElement()']");
