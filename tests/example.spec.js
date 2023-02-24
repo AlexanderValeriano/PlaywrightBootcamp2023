65655a80a0c6135e15096d07c2e329b4c6307a0d
valeriano alexander <valerianoalexander@gmail.com>
2023-02-24 10:20:34 +0100

    197 Introduction to assertions

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -7,14 +7,22 @@ test.describe.parallel("Smoke Tests", () => {
   test.afterEach(async ({ page }) => {
     console.log("This test is finished");
   });
+  //     .locator("//a[@href='https://mamutuelle.interiale.fr']")
   test("Testing Selectors", async ({ page }) => {
+    await expect
+      .soft(
+        page.locator("//header/div[1]/div[1]/div[1]/div[2]/div[1]/div[6]/a[1]")
+      )
+      .toHaveText("Espace adhérent");
+
     await page
       .locator(
         "//a[@class='btn py-5 px-4 text-grey-dark'][normalize-space()='Intérieur']"
       )
       .click();
     await expect(
-      page.locator("//div[contains(text(),'intérieur')]")
+      page.locator("//div[contains(text(),'intérieur')]"),
+      "Custom expect message"
     ).toHaveText("intérieur");
   });
 });
